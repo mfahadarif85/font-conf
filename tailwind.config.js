53f79039d7332f08fe7464528fae064598ff0d73
mfahadarif85 <mfahadarif@gmail.com>
2023-03-08 13:47:03 +0500

    changed tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
       fontFamily: {
