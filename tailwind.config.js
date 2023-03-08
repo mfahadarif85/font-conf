1a8847b77782b7933001fc3b02e491d3a3408ac6
mfahadarif85 <mfahadarif@gmail.com>
2023-03-08 13:29:39 +0500

    updated styling and added responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Fira Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#5555ff",
+      },
+    },
   },
   plugins: [],
 };
